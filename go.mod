25d2ff4e53384c93d71498074da86869a794e7aa
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-09-07 15:33:23 +0100

    ✨ Add web server

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/connormaglynn/learning-golang
-
-go 1.21.1
---
+++ web-server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!🌎")
+	})
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./home.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
