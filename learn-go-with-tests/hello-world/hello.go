d260c879577a1459ed8bf81ea0623b7b47adf5c4
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-09-17 20:00:34 +0100

    ✨ Add language option to hello function

--- learn-go-with-tests/hello-world/hello.go
+++ learn-go-with-tests/hello-world/hello.go
@@ -1,10 +1,14 @@
 package main
 
 const englishHello = "Hello, "
+const spanishHello = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, langauge string) string {
 	if name == "" {
 		name = "World"
+	}
+	if langauge == "Spanish" {
+		return spanishHello + name
 	}
 	return englishHello + name
 }
--- learn-go-with-tests/hello-world/hello_test.go
+++ learn-go-with-tests/hello-world/hello_test.go
@@ -6,11 +6,16 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	t.Run("returns Hello, Connor when Connor is passed", func(t *testing.T) {
-		assert.Equal(t, "Hello, Connor", Hello("Connor"))
+	t.Run("returns 'Hello, World' when empty string and no language is passed", func(t *testing.T) {
+		assert.Equal(t, "Hello, World", Hello("", ""))
 	})
 
-	t.Run("returns Hello, World when empty string is passed", func(t *testing.T) {
-		assert.Equal(t, "Hello, World", Hello(""))
+	t.Run("returns 'Hello, Connor' when 'Connor' and no language is passed", func(t *testing.T) {
+		assert.Equal(t, "Hello, Connor", Hello("Connor", ""))
 	})
+
+	t.Run("returns 'Hola, World' when empty string and 'Spanish'", func(t *testing.T) {
+		assert.Equal(t, "Hola, World", Hello("", "Spanish"))
+	})
+
 }
